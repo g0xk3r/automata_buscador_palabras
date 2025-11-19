396ce9968d58a1890bbbf31dfdae09671cbc467f
g0xk3r <gonzalo.sv@outlook.com>
2025-11-19 01:07:06 -0600

    falta la clase que corre ambos automatas

---
+++ dfa.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+from bs4 import BeautifulSoup
+from graphviz import Digraph
+from typing import Set, Dict, Tuple, List, Optional
+
+class DFA:
+    def __init__(self, estados: Set[int], estado_inicial: int, estados_finales: Set[int], alfabeto: Set[str], transiciones: Dict[int, Dict[str, int]], mapa_estados: Optional[Dict[frozenset, int]] = None):
+        self.estados = estados
+        self.estado_inicial = estado_inicial
+        self.estados_finales = estados_finales
+        self.alfabeto = alfabeto
+        self.transiciones = transiciones
+        self.mapa_estados = mapa_estados
+
+    def mostrar_grafo(self):
+        punto = Digraph(format='png')
+        punto.attr(rankdir='LR')
+        punto.node('start', shape='none')
+        for estado in self.estados:
+            if estado in self.estados_finales:
+                punto.node(str(estado), shape='doublecircle')
+            else:
+                punto.node(str(estado), shape='circle')
+
+        punto.edge('start', str(self.estado_inicial))
+
+        for estado_inicial, mapa_transiciones in self.transiciones.items():
+            for simbolo, estado_final in mapa_transiciones.items():
+                punto.edge(str(estado_inicial), str(estado_final), label=simbolo)
+
+        return punto
--- main.py
+++ main.py
@@ -1,6 +1,100 @@
+import sys
+from collections import deque
+from bs4 import BeautifulSoup
+from graphviz import Digraph
+from typing import Set, Dict, Tuple, List, Optional
+from dfa import DFA
 class NFA:
-    def __init__(self, estados: set[int], estado_inicial: int, estado_aceptacion: set[int], alfabeto: set[str], transiciones: dict[tuple[int, str], set[int]]):
+    def __init__(self, estados: Set[int], estado_inicial: int, estado_aceptacion: Set[int], alfabeto: Set[str], transiciones: Dict[Tuple[int, str], Set[int]]):
         self.estados = estados
         self.estado_inicial = estado_inicial
         self.estado_aceptacion = estado_aceptacion
         self.alfabeto = alfabeto
+        self.tranciones = transiciones
+
+    def cerradura_epsilon(self, estados: Set[int]):
+        pila = list(estados)
+        cerradura = set(estados)
+
+        while pila:
+            estado_actual = pila.pop()
+            transiciones_epsilon = self.tranciones.get((estado_actual, ' '), set())
+            for estado in transiciones_epsilon:
+                if estado not in cerradura:
+                    cerradura.add(estado)
+                    pila.append(estado)
+        return cerradura
+
+    def mover(self, estados: Set[int], simbolo: str):
+        estados_alcanzables = set()
+        for estado in estados:
+            transiciones_simbolo = self.tranciones.get((estado, simbolo), set())
+            estados_alcanzables.update(transiciones_simbolo)
+        return estados_alcanzables
+
+    def convertir_a_dfa(self):
+        estados_dfa = set()
+        estado_inicial_dfa = frozenset(self.cerradura_epsilon({self.estado_inicial}))
+        estados_finales_dfa = set()
+        transiciones_dfa = {}
+        mapa_estados_dfa: Dict[frozenset[int], int] = {estado_inicial_dfa: 0}
+        id_contador_estados_dfa = 0
+        cola = deque([estado_inicial_dfa])
+        estados_dfa.add(estado_inicial_dfa)
+        while cola:
+            estado_actual_nfa = cola.popleft()
+            id_estado_actual_dfa = mapa_estados_dfa[estado_actual_nfa]
+            if not estado_actual_nfa.isdisjoint(self.estado_aceptacion):
+                estados_finales_dfa.add(id_estado_actual_dfa)
+
+            for simbolo in self.alfabeto:
+                siguientes_estados_nfa = self.mover(estado_actual_nfa, simbolo)
+                siguientes_estados_nfa_cerradura = frozenset(self.cerradura_epsilon(siguientes_estados_nfa))
+                if not siguientes_estados_nfa_cerradura:
+                    continue
+                if siguientes_estados_nfa_cerradura not in estados_dfa:
+                    estados_dfa.add(siguientes_estados_nfa_cerradura)
+                    cola.append(siguientes_estados_nfa_cerradura)
+                    id_contador_estados_dfa += 1
+                    mapa_estados_dfa[siguientes_estados_nfa_cerradura] = id_contador_estados_dfa
+                id_siguiente_estado_dfa = mapa_estados_dfa[siguientes_estados_nfa_cerradura]
+                if id_estado_actual_dfa not in transiciones_dfa:
+                    transiciones_dfa[id_estado_actual_dfa] = {}
+                transiciones_dfa[id_estado_actual_dfa][simbolo] = id_siguiente_estado_dfa
+        return DFA(
+            estados=set(mapa_estados_dfa.values()),
+            estado_inicial=mapa_estados_dfa[estado_inicial_dfa],
+            estados_finales=estados_finales_dfa,
+            alfabeto=self.alfabeto,
+            transiciones=transiciones_dfa,
+            mapa_estados=mapa_estados_dfa
+        )
+
+    @staticmethod
+    def a_palabras_clave(palabras_clave: List[str]):
+        estado_inicial = 0
+        estados = {0}
+        estados_finales = {}
+        transiciones = {}
+        alfabeto = set()
+        nuevo_contador_estado = 0
+
+        for palabra in palabras_clave:
+            estado_actual = estado_inicial
+            for simbolo in palabra:
+                alfabeto.add(simbolo)
+                if (estado_actual, simbolo) in transiciones:
+                    estado_actual = list(transiciones[(estado_actual, simbolo)])[0]
+                else:
+                    nuevo_contador_estado += 1
+                    estados.add(nuevo_contador_estado)
+                    transiciones[(estado_actual, simbolo)] = {nuevo_contador_estado}
+                    estado_actual = nuevo_contador_estado
+            estados_finales.add(estado_actual)
+        return NFA(
+            estados=estados,
+            estado_inicial=estado_inicial,
+            estado_aceptacion=estados_finales,
+            alfabeto=alfabeto,
+            transiciones=transiciones
+        )
